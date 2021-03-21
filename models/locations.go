f169dbf4948e477ca49d0d92d15531aded2c3794
Nick Fox <njfox@users.noreply.github.com>
2021-03-20 20:15:22 -0400

    Update models and location endpoint to handle  breaking API changes

--- models/locations.go
+++ models/locations.go
@@ -8,12 +8,22 @@ type PurchaseLocation struct {
 
 // Models the Location object.
 type Location struct {
-	Name    string `json:"name"`
-	Symbol  string `json:"symbol"`
-	Type    string `json:"type"`
-	X       int    `json:"x"`
-	Y       int    `json:"y"`
-	Anomaly string `json:"anomaly"`
+	Name            string       `json:"name"`
+	Symbol          string       `json:"symbol"`
+	Type            string       `json:"type"`
+	X               int          `json:"x"`
+	Y               int          `json:"y"`
+	Anomaly         string       `json:"anomaly"`
+	AnsibleProgress int          `json:"ansibleProgress"`
+	Ships           []DockedShip `json:"ships"`
+	DockedShips     int          `json:"dockedShips"`
+}
+
+// Models the DockedShip object returned from a Location
+type DockedShip struct {
+	ID       string `json:"shipId"`
+	Username string `json:"username"`
+	Type     string `json:"shipType"`
 }
 
 // Models a Location object that has a Market.
